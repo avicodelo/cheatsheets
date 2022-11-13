fa118b36bb5fcb92b74de171a4d96f4bfa2381a4
Aitor <avicolo@hotmail.com>
2022-11-14 00:26:09 +0100

    DOM JS v2.1

--- JS/DOM_JS.js
+++ JS/DOM_JS.js
@@ -18,14 +18,44 @@
     
 //**ACCESO AL DOM**//
     //Selección por ID (me dará un unico elemento)
-        document.getElementById("mainTable");
+        const grandParent = document.getElementById("grandparent");
+        console.log(grandParent);
 
-    //Selección por etiqueta HTML (me dará varios elementos en un array)
-        const family = document.getElementsByTagName("h1");
-        console.log(family);
+    //Selección por etiqueta HTML 
+        //El output es un HTMLCollection (diferente a un array)
+            const family = document.getElementsByTagName("div");
+            console.log(family);
+
+        //Para acceder a los elementos, tenemos dos maneras
+            // 1) parseando el iterable HTMLColl. y aplicando forEach
+                Array.from(family).forEach(item => console.log(item));
+
+            // 2) directamente con for...of
+                for (let familyMembers of family){
+                    console.log(familyMembers);
+                }
 
     //Selección por clase (me dará varios elementos en un array)
-        document.getElementsByClassName("container");
+        //El output es un HTMLCollection (diferente a un array)
+            const parents = document.getElementsByClassName("parent");
+        //Guardar los diferentes elementos en constantes 
+            // similar a const parent1 = parents [0]
+                const parent1 = document.getElementsByClassName("parent")[0];
+            // similar a const parent2 = parents [1]
+                const parent2 = document.getElementsByClassName("parent")[1];
+
 
     //Selección por un selector completo
-        document.querySelector("a.mainlink");
+        //Se puede aplicar cualquier selector igual que en css
+        //Sólo coge el primer elemento que coincida la indicación 
+            let element = document.querySelector(".grandparent>.parent>#child4");
+            console.log(element);
+
+        //Coge todos los elementos que coincidan
+            let elementAll = document.querySelectorAll(".grandparent>.parent div");
+            console.log(elementAll);  
+
+
+//**MODIFICANDO EL DOM**//
+    //Aplicar estilos CSS desde JS (se suprimen los guiones de las propiedades CSS)
+        parent1.style.backgroundColor = "#333";
