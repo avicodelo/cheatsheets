21891bc91074f4e273dfc85395bc64d4d46ccd35
Aitor <avicolo@hotmail.com>
2022-12-22 19:59:23 +0100

    Mongo added

---
+++ Mongo.md
@@ -0,0 +1,142 @@
+### SHOW
+
+    - Mostrar las bases de datos
+        show dbs
+
+    - Mostrar las colecciones
+        show collections
+
+### USE
+
+    - Seleccionar una base de datos. Si no existe, la crea
+        use nombre_database
+
+# DB.
+## Hace referencia a la base de datos de referencia (en la que nos encontramos)
+    - Eliminar la base de datos de que en la que nos encontramos*/
+        db.dropDatabase()
+    - Crear una colección en la base de datos
+        db.createCollection("nombre_colección")
+
+# DB.COLLECTION
+## Hace referencia a la colección nombrada
+
+### RENAMECOLLECTION
+    - Renombrar una colección
+        db.old_name.renameCollection("new_name")
+   
+### INSERT
+
+        - Introducir nuevo registro (siempre formato json). Si la colección no existe, la crea
+            db.collection_name.insert({name: "Curro", age: 20, city: "Granada"})
+        
+        -Introducir múltiples registros
+            db.collection_name.insertMany([{regisro 1}, {registro 2}, etc ])
+
+### FIND        
+    - Mostrar todos los registros
+        db.collection_name.find()
+
+    - Mostrar los resultados en forma de json
+        db.collection_name.find().pretty()
+
+    - Se pueden filtrar los resgistros introduciendo un objeto en el argumento
+        db.collection_name.find({name: "Curro"})
+
+    - Mostrar sólo la primera coincidencia
+        db.collection_name.findOne({age:20})
+
+    - Limitar el número de campos mostrados (en el ejemplo sólo mostrará el nombre).
+      El _id lo mostrará por defecto si no establecemos false
+        db.collection_name.find({name: "Curro"}, {name: true , _id: false})
+
+    - Si tenenemos un array y queremos saber si contien algo, tratarlo como una propiedad normal (contacts es un array).
+        db.users.find({contacts: allMembers[1]._id})
+
+### SORT
+    - Unido al find(), ordena por la propiedad indicada y le otorgamos el orden con la prioridad (este caso 1)
+        db.collection_name.find().sort({name: 1})
+
+### COUNT
+    - Contar el número de registros que coinciden
+        db.collection_name.count({age: 20})
+
+### LIMIT
+    - Limita la cantidad de registros que devuelve
+        db.collection_name.find().sort({age: 1}).limit(2)
+
+### SKIP
+    - Establece un punto de partida para mostrar info (en el ejemplo empezaría a partir de registro 3). Útil para paginar
+        db.collection_name.find().skip(3).limit(10)
+
+### $EXISTS
+    - Detectar si una propiedad existe
+        db.collection_name.find(name: {$exists: true})
+
+### $UNSET
+    - Eliminar un campo
+        db.collection_name.find({name:"Curro"}, {$unset: {city:""}})
+
+### $SIZE
+    - Comparar tamaño de un array
+        db.chatData.find({members: {$size: 2}})
+
+### $COMPARADORES
+    - $gt (greater than)
+    - $gte (greater than or equal)
+    - $lt (less than)
+    - $lte (less than or equal)
+    - $ne (not equal)
+
+### UPDATE
+    - Sustituye todas las propiedades por la que le indico tras filtrarme con el primer parámetro. Sólo primer registro 
+        db.collection_name.update({name: "Curro"}, {name:"santiago"})
+
+    - Mantiene todos los parámetros intactos excepto el que le indico. Sólo primer registro
+        db.collection_name.update({name:"Curro"}, {$set: {name:"Carlos"}})
+
+    - Modifica los datos numéricos sumando lo que le indico (en este caso: +5). Sólo primer registro
+        db.collection_name.update({name: "Curro"}, {$inc {age: 5}})
+
+    - Modifica todos los registros que hacen match
+        db.collection_name.update({name: "Curro"}, {age: 5}, {multi: true})
+
+    - Todos los registros hacen match dejando el objeto vacío
+        db.collection_name.update({}, {$set {age: 5}}, {multi: true})
+    
+### REMOVE
+    db.collection_name.remove()
+    
+### Determinar constantes
+    - constante de un único dato
+        const product = db.collection_name.findOne({productName : "cacerola"})
+
+    - constante de varios datos transformados a un array
+        const products = db.collection_name.find().toArray()
+
+### Nomenclatura punto
+    - Acceder a una propiedad de un objeto que está dentro de un registro. Poner comillas
+        db.collection_name.find({"address.city" : "Madrid"})
+
+    - Acceder a un array con propiedades
+        db.collection_name.update({name : "Curro"}, {$set{trolley : products[1]._id}})
+
+### Volcar y guardar base de datos
+    - Copiar base datos
+        mongodump -h <host:port> -d <folder_name> --o <route_directory_where_save>
+            ej: mongodump -h 127.0.0.1:27017 -d users --out ~/documents
+
+    - Importar la base de datos entera
+        mongorestore -h <host:port> <directory or file to restore>
+            ej: mongorestore -h 127.0.0.1:27017 ~/folder/database_directory
+
+    - Importar colecciones dentro de una base de datos
+        mongoimport --db <dbName> --collection <collection_name> --file <fileName.json>
+            ej: mongoimport --db <carShop> --collection <cars> --file cars.json
+
+### REGEX
+    - . = cualquier cosa
+    - * = que contenga todos los caracteres indicados
+    - ^ = que empieze por
+    - $ = que termine por
+    - [] = que esté en rango 
--- MySQL.sql
+++ MySQL.sql
@@ -16,6 +16,12 @@
     
     /*Fecha*/
         date
+            /*formato de fecha*/
+                YYYY-MM-DD
+            /*extraer algunos de los parámetros por separado*/
+                YEAR(nombre_columna);
+                MONTH(nombre_columna)
+                DAY(nombre_columna);
    
     /*Fecha y hora*/
         datetime
@@ -48,6 +54,12 @@
 
             /* mostrar permisos de un usuario*/
                 SHOW GRANTS FOR 'nuevo_usuario'@'localhost';
+
+            /*Mostrar la lista de procesos activos de las bases de datos*/
+                SHOW PROCESSLIST;
+
+            /*Matar un proceso abierto*/
+                KILL id_sesion;
 
    /*Mostrar estructura de una tabla*/
         DESCRIBE nombre_tabla;
@@ -129,11 +141,17 @@
             SELECT 
             /*Seleccionar todos los campos*/
                 SELECT * FROM nombre_tabla;
-                    /*Especificar los datos que queremos que nos enseñe (en el ejemplo, email, nombre de clientes)*/
-                        SELECT email, nombre FROM costumers;
+                    /*Se puede especificar la tabla de la que queremos ver todos los datos. Útil cuando juntamos varias tablas*/
+                        SELECT nombre_tabla.* from nombre_tabla;
 
-            /*Seleccionar algunos campos*/
-                SELECT nombre, apellidos FROM nombre_tabla;
+            /*Especificar los datos que queremos que nos enseñe (en el ejemplo, email, nombre de clientes)*/
+                SELECT email, nombre FROM nombre_tabla;
+
+            /*Establecer un límite de registros mostrados (en este caso 4)*/
+                SELECT * FROM nombre_tabla LIMIT 4;
+
+            /*Seleccionar datos con id iguales a la lista establecida después de IN*/
+                SELECT * FROM nombre_tabla WHERE id IN (1,2,3);
 
             /*Ordenar los resultados por orden ascendente o descendente*/
                 SELECT * FROM nommbre_tabla ORDER BY id DESC;
@@ -158,13 +176,30 @@
                 SELECT * FROM nombre_tabla WHERE id >= 1 AND nombre = 'Pedro';
 
             /*Buscar coincidencias que contengan un texto*/
-                SELECT * FROM user_type WHERE nombre LIKE '%pep%';
+                SELECT * FROM nombre_tabla WHERE nombre LIKE '%pep%';
 
             /*Devolver un valor número con la cantidad de registros*/
                 SELECT COUNT(*) FROM nombre_tabla;
 
+            /*Devolver la media de los registros*/
+                SELECT AVG(nombre_columna) FROM nombre_tabla;
+
+            /*Devolver la suma de los registros*/
+                SELECT SUM(nombre_columna) FROM nombre_tabla;
+
+            /*Devolver el valor máximo de los registros*/
+                SELECT MAX(nombre_columna) FROM nombre_tabla;
+
+            /*Devolver el valor mínimo de los registros*/
+                SELECT MIN(nombre_columna) FROM nombre_tabla;
+
+            /*Devolver diferencia de fechas. Resta primera menos segunda*/
+                SELECT DATEDIFF(fecha_1, fecha_2) FROM nombre_tabla;
+
             /*Asignar un alias a una columna*/
                 SELECT id as identificador, nombre as como_me_llamo FROM nombre_tabla;
+                /*opción alternativa: poner un espacio*/
+                    SELECT id identificador, nombre como_me_llamo; 
 
             /*Ejecutar consultas dentro de otras consultas*/
                 SELECT nombre, apellidos FROM usuarios WHERE id IN 
@@ -175,7 +210,7 @@
                             id = 10);
 
             /*Tablas relacionadas con JOIN*/
-                /*Consultar dos tablas unidas por su clave foranea incluyendo solo matches conectadas*/
+                /*Consultar dos tablas unidas por su clave foranea incluyendo solo matches conectadas (foreign key = primary key)*/
                     SELECT * FROM nombre_tabla INNER JOIN otra_tabla ON otra_tabla.id = nombre_tabla.estado;
 
                 /*Todas las filas conectadas y todas las filas sueltas de la tabla de la izquierda (primera). Valores null en las celdas de la otra tabla*/
@@ -186,20 +221,30 @@
 
                 /*Todas las conectadas y no conectadas. No en mysql*/
                     SELECT * FROM nombre_tabla FULL JOIN otra_tabla ON otra_tabla.id = nombre_tabla.estado;
+
+                /*Self Join para hacer referencia a una tabla con jerarquía*/
+                    /*ex1 y ex2 son los alias diferentes que se aplicarán a la misma tabla para referenciar la relación*/
+                        SELECT ex1.nombre_columna, ex2.nombre_columna 
+                            FROM nombre_tabla ex1 
+                                JOIN nombre_tabla ex2 ON ex1.primary_key = ex2.foreign_key;
+                    /*Ejemplo práctico*/
+                        SELECT clase1.clase_coche, clase2.clase_coche   
+                            FROM tipo_coches clase1
+                                JOIN tipo_coches clase2 ON clase1.clase_coche_id = clase2.id;
                     
             /*Ejemplo*/
             SELECT name, last_name, brand, price FROM clients left join bikes on bikes.id = clients.bike_id where name="Jane";
 
         /*Insertar registros en la tabla*/
             INSERT
-            /**/
+            /*Introduce los valores en la tabla nombrada. Opción 1*/
                 INSERT INTO nombre_tabla SET id=NULL, nombre='Alfredo';
 
-            /**/
-                INSERT INTO table_name (column1, column2, column3, ...) VALUES (value1, value2, value3, ...); 
+            /*Introduce los valores en la tabla nombrada. Opción 2*/
+                INSERT INTO nombre_tabla (column1, column2, column3, ...) VALUES (value1, value2, value3, ...); 
 
-            /**/
-                INSERT INTO nombre_tabla values("value1", "value2");
+            /*Introduce los valores en la tabla nombrada. Opción 3*/
+                INSERT INTO nombre_tabla VALUES("value1", "value2", ...);
 
         /* Modifica datos existentes dentro de la tabla*/
             UPDATE
@@ -214,6 +259,27 @@
                 
             /*borrar un dato*/
                 DELETE FROM nombre_tabla WHERE id=1;
+
+/*DCL (Data Control Language)*/
+    /*DCL se aplica a la gestión de permisos*/
+
+        /*Otorgar permisos*/
+            /*indicar el comando autorizado (este caso CREATE)*/
+            GRANT CREATE ON *.* TO "username"@"localhost";
+
+        /*Otorgar permisos*/
+            REVOKE ALL ON *.* FROM "username"@"localhost";
+
+        /*Actualizar los cambios en los permisos sin salir del servidor*/
+            FLUSH PRIVILEGES;
+
+        /*Niveles de permisos*/
+            /*Globales*/
+                *.*
+            /*Base de datos*/
+                nombre_base_datos.*
+            /*Tablas*/
+                nombre_base_datos.nombre_tabla
 
 /*COMANDOS DE EJEMPLO*/
 
